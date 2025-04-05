ef8f7b2e87d5ce4d0c077efe19b45b02689afdf0
facu1401 <fjgs0420@gmail.com>
2025-04-05 17:23:52 +0200

    adding new modul

--- M3L1/main.py
+++ M3L1/main.py
@@ -1,15 +1,23 @@
-import discord
-from discord.ext import commands
+from flask import Flask
+import random
+app = Flask(__name__)
 
-intents = discord.Intents.default()
-intents.message_content = True
+facts_list = ["La mayoría de las personas que sufren adicción tecnológica experimentan un fuerte estrés cuando se encuentran fuera del área de cobertura de la red o no pueden utilizar sus dispositivos",
+    "Según un estudio realizado en 2018, más del 50% de las personas de entre 18 y 34 años se consideran dependientes de sus smartphones.El estudio de la dependencia tecnológica es una de las áreas más relevantes de la investigación científica moderna",
+    "Según un estudio de 2019, más del 60% de las personas responden a mensajes de trabajo en sus smartphones en los 15 minutos siguientes a salir del trabajo",
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+    "Una forma de combatir la dependencia tecnológica es buscar actividades que aporten placer y mejoren el estado de ánimo Elon Musk afirma que las redes sociales están diseñadas para mantenernos dentro de la plataforma, para que pasemos el mayor tiempo posible viendo contenidos",
 
-@bot.event
-async def on_ready():
-    print('We have logged in as {bot.user}')
+    "Elon Musk también aboga por la regulación de las redes sociales y la protección de los datos personales de los usuarios. Afirma que las redes sociales recopilan una enorme cantidad de información sobre nosotros, que luego puede utilizarse para manipular nuestros pensamientos y comportamientos",
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send('Hola, soy un bot {bot.user}!')
+    "Las redes sociales tienen aspectos positivos y negativos, y debemos ser conscientes de ambos cuando utilicemos estas plataformas",
+
+ ]
+@app.route("/random_fact")
+def datos():
+    return f'<h1>DATOS CURIOSOS: </h1><p>{random.choice(facts_list)}</p>'
+@app.route("/")
+def index():
+    return '<h1>Dependencias tenológicas</h1><a href="/random_fact">¡Ver un dato aleatorio!</a>'
+
+app.run(debug=True)
